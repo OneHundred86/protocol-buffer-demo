723a759a0c07310df0a67d8e5b1c07a2a1834840
chenguangjun <chen.gj@qq.com>
2023-05-07 23:45:16 +0800

    解决tcp粘包问题

--- tcp_client.php
+++ tcp_client.php
@@ -24,9 +24,9 @@ run(function () use($ip, $port) {
     $bar->setHobbies(["basketball", "football"]);
 
     // Protocol Buffers本身并没有提供解决TCP粘包问题的机制，在这里使用固定长度的消息头来解决粘包问题：消息头用于指定后续消息的长度，这里定义4个字节作为消息头。
+    // 消息长度（固定4字节） + 路由（自定义，固定4字节） + 消息体（系列化）
     // 定义协议，前4个字节表示消息头（用于路由），后面内容表示消息体
     // 1表示使用 \Pt\Message\Foo 作为消息体
-    // 消息长度（固定4字节） + 路由（自定义，固定4字节） + 消息体（系列化）
     $bin = \Protobuf\Test\Lib\Pt::int2bin(1) . $bar->serializeToString();
     $msgBin = \Protobuf\Test\Lib\Pt::int2bin(strlen($bin)) . $bin;
     $client->send($msgBin);
